ea4f7f40308bd3fccc543ce6b1b6186855ff8305
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 02:12:40 +0300

    add js change

--- Assets/data.js
+++ Assets/data.js
@@ -9,7 +9,17 @@ function report(){
         data [2]["shed_C"] = parseInt(document.getElementById("shed_C").value);
         data [3]["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
-    let sumData = data.shed_A + data.shed_A + data.shed_C + data.shed_D;  
+    let sumData = data.shed_A + data.shed_A + data.shed_C + data.shed_D; 
+    
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
+
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
+    
         
 
 
