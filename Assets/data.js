ee00856aa6e6ac55649e87f978ffebe71c5c6285
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 02:33:32 +0300

    add js change

--- Assets/data.js
+++ Assets/data.js
@@ -3,10 +3,10 @@ function report(){
 
     let data = [] ;
 
-        data [0]["shed_A"] = parseInt(document.getElementById("shed_A").value);
-        data [1]["shed_B"] = parseInt(document.getElementById("shed_B").value);
-        data [2]["shed_C"] = parseInt(document.getElementById("shed_C").value);
-        data [3]["shed_D"] = parseInt(document.getElementById("shed_D").value);
+        data [0] = ["shed_A"] = parseInt(document.getElementById("shed_A").value);
+        data [1] = ["shed_B"] = parseInt(document.getElementById("shed_B").value);
+        data [2] = ["shed_C"] = parseInt(document.getElementById("shed_C").value);
+        data [3] = ["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
     let sumData = data.shed_A + data.shed_A + data.shed_C + data.shed_D; 
     
