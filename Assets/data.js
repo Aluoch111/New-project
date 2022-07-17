b75eda6ee464a8df2c01520fa46c32fd7acb5f7d
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 02:16:34 +0300

    initial commit

--- Assets/data.js
+++ Assets/data.js
@@ -13,12 +13,12 @@ function report(){
     
     document.getElementById("outputData").innerHTML += "<Br>"
 
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres/day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres/day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres/per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres/per day</p>";
 
-    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres/per day</p>";
     
         
 
