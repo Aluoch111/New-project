80beee3b1f90d9e94dcfe88bc0e8d6514a4441a4
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 02:00:50 +0300

    add data input

--- Assets/data.js
+++ Assets/data.js
@@ -1,1 +1,20 @@
-function report(){}
+function report(){
+
+    Event.preventDefault();
+
+    let data = [] ;
+
+        data [0]["shed A"] = parseInt(document.getElementById("shed A").value);
+        data [1]["shed B"] = parseInt(document.getElementById("shed B").value);
+        data [2]["shed C"] = parseInt(document.getElementById("shed C").value);
+        data [3]["shed D"] = parseInt(document.getElementById("shed D").value);
+        
+
+
+
+
+
+
+
+
+}
