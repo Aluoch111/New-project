8ba284158510be6076acf785e131cfd6ff234c56
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 02:17:47 +0300

    initial commit

--- Assets/data.js
+++ Assets/data.js
@@ -15,10 +15,10 @@ function report(){
 
     document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres/day</p>";
     document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres/day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres/per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres/per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres/day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres/day</p>";
 
-    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres/per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres/day</p>";
     
         
 
