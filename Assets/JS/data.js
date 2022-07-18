8bd65c964b4fd9a43a9cf0804d3bbdcfb7ae1287
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-07-18 03:16:51 +0300

    initial commit

--- Assets/JS/data.js
+++ Assets/JS/data.js
@@ -4,10 +4,10 @@ function report(){
 
     let data = [] ;
 
-        data [0] = ["shed_A"] = parseInt(document.getElementById("shed_A").value);
-        data [1] = ["shed_B"] = parseInt(document.getElementById("shed_B").value);
-        data [2] = ["shed_C"] = parseInt(document.getElementById("shed_C").value);
-        data [3] = ["shed_D"] = parseInt(document.getElementById("shed_D").value);
+        data [1] = ["shed_A"] = parseInt(document.getElementById("shed_A").value);
+        data [2] = ["shed_B"] = parseInt(document.getElementById("shed_B").value);
+        data [3] = ["shed_C"] = parseInt(document.getElementById("shed_C").value);
+        data [4] = ["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
     let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D; 
     
